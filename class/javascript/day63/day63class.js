0f4d41021052007fbad505fb9cb3532607caadf9
DavidBeria <daviberia11@gmail.com>
2024-04-26 20:31:23 +0400

    saklaso

---
+++ class/javascript/day63/day63class.js
@@ -0,0 +1,8 @@
+let user = {
+    name: 'Davit',
+    surname: 'Beria',
+    adress: 'rustaveli',
+    password: '332241(fake)',
+}
+
+console.log('ჩემი სახელია ' + user.name + "ჩემი გვარია " + user.surname + "მე ვცხოვრობ " + user.adress + "ჩემი პაროლია " + user.password)
---
+++ test.js
@@ -0,0 +1,5 @@
+let band = "Beatles";
+let song = "Yesterday";
+song = "Let it be";
+console.log(song);
+console.log(band);
