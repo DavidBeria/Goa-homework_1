ffbe8d42db12b1c861919fba98b4e60f4b5bcc33
DavidBeria <daviberia11@gmail.com>
2024-05-23 19:22:43 +0400

    davaleba

---
+++ class/javascript/day65/day65classwork/day65class.js
@@ -0,0 +1,16 @@
+// function sayHi(name) {
+//     console.log("hello ${name}, how are you")
+// }
+// sayHi()
+
+
+
+
+
+
+
+function sayHi(name) {
+    return "hello ${name}, how are you"
+}
+
+console.log(sayHi(davit))
---
+++ class/javascript/day71homework/day71homework.js
@@ -0,0 +1,24 @@
+let number = document.getElementById('num')
+let plus = document.getElementById('plus')
+let reset = document.getElementById('reset')
+let minus = document.getElementById('minus')
+let numberJs = 0
+
+function numberdis() {
+    number.textContent = numberJs
+}
+
+plus.addEventListener('click', () =>{
+    numberJs++
+    numberdis()
+})
+
+minus.addEventListener('click', () =>{
+    numberJs--
+    numberdis()
+})
+
+reset.addEventListener('click', () =>{
+    let numberJs = 0
+    numberdis()
+})
